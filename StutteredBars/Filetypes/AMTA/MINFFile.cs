378a0b1515f451aa9e735e7525980893da0767fc
ash <111416629+ashbinary@users.noreply.github.com>
2025-02-04 15:21:10 -0700

    InstrumentTable work

--- StutteredBars/Filetypes/AMTA/MINFFile.cs
+++ StutteredBars/Filetypes/AMTA/MINFFile.cs
@@ -65,6 +65,29 @@ public struct MINFFile
         public uint Reserve4;
     }
 
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MINFPair
+    {
+        public uint SampleOffset;
+        public uint Reserve1;
+    }
+
+    public struct MINFInstrument
+    {
+        public uint InstrumentNameOffset;
+        public ushort Reserve0;
+        public ushort Reserve1;
+        public uint Reserve2;
+        public string InstrumentName;
+    }
+
+    public struct MINFInstrumentInfo
+    {
+        public uint Reserve0;
+        public uint InstrumentOffset;
+        public MINFInstrument Instrument;
+    }
+
     public struct MINFTable0
     {
         public ushort EntryCount;
@@ -85,6 +108,27 @@ public struct MINFFile
         public ushort Reserve0;
         public MINFTable2Entry[] Entries; 
     }
+    
+    public struct MinfOffsetTable
+    {
+        public ushort OffsetCount;
+        public ushort Reserve0;
+        public uint[] Offsets;
+    }
+
+    public struct MinfPairTable
+    {
+        public ushort PairCount;
+        public ushort Reserve0;
+        public MINFPair[] Pairs;
+    }
+
+    public struct MinfInstrumentTable
+    {
+        public ushort InstrumentCount;
+        public ushort Reserve0;
+        public MINFInstrumentInfo[] InstrumentInfo;
+    }
 
     private long BaseAddress;
 
@@ -92,6 +136,9 @@ public struct MINFFile
     public MINFTable0 Table0;
     public MINFTable1 Table1;
     public MINFTable2 Table2;
+    public MinfOffsetTable OffsetTable;
+    public MinfPairTable PairTable;
+    public MinfInstrumentTable InstrumentTable;
 
     public MINFFile(ref FileReader minfReader)
     {
@@ -100,6 +147,11 @@ public struct MINFFile
         Info = MemoryMarshal.AsRef<MINFInfo>(
             minfReader.ReadBytes(Unsafe.SizeOf<MINFInfo>())
         );
+
+        if (Info.InstrumentInfoTableOffset != 0)
+        {
+            Console.WriteLine("oh my instrumentation");
+        }
 
         minfReader.Position = BaseAddress + Marshal.OffsetOf<MINFInfo>("Table0Offset") + Info.Table0Offset;
 
@@ -112,6 +164,8 @@ public struct MINFFile
                 minfReader.ReadBytes(Unsafe.SizeOf<MINFTable0Entry>())
             );
 
+        minfReader.Position = BaseAddress + Marshal.OffsetOf<MINFInfo>("Table1Offset") + Info.Table1Offset;
+
         Table1.EntryCount = minfReader.ReadUInt16();
         Table1.Reserve0 = minfReader.ReadUInt16();
         Table1.Entries = new MINFTable1Entry[Table1.EntryCount];
@@ -121,6 +175,8 @@ public struct MINFFile
                 minfReader.ReadBytes(Unsafe.SizeOf<MINFTable1Entry>())
             );
 
+        minfReader.Position = BaseAddress + Marshal.OffsetOf<MINFInfo>("Table2Offset") + Info.Table2Offset;
+
         Table2.EntryCount = minfReader.ReadUInt16();
         Table2.Reserve0 = minfReader.ReadUInt16();
         Table2.Entries = new MINFTable2Entry[Table2.EntryCount];
@@ -129,5 +185,73 @@ public struct MINFFile
             Table2.Entries[i] = MemoryMarshal.AsRef<MINFTable2Entry>(
                 minfReader.ReadBytes(Unsafe.SizeOf<MINFTable2Entry>())
             );
+
+        minfReader.Position = BaseAddress + Marshal.OffsetOf<MINFInfo>("PairTableOffset") + Info.PairTableOffset;
+
+        PairTable.PairCount = minfReader.ReadUInt16();
+        PairTable.Reserve0 = minfReader.ReadUInt16();
+        PairTable.Pairs = new MINFPair[PairTable.PairCount];
+
+        for (int i = 0; i < PairTable.PairCount; i++)
+            PairTable.Pairs[i] = MemoryMarshal.AsRef<MINFPair>(
+                minfReader.ReadBytes(Unsafe.SizeOf<MINFPair>())
+            );
+
+        minfReader.Position = BaseAddress + Marshal.OffsetOf<MINFInfo>("OffsetTableOffset") + Info.OffsetTableOffset;
+
+        OffsetTable.OffsetCount = minfReader.ReadUInt16();
+        OffsetTable.Reserve0 = minfReader.ReadUInt16();
+        OffsetTable.Offsets = new uint[OffsetTable.OffsetCount];
+
+        for (int i = 0; i < OffsetTable.OffsetCount; i++)
+            OffsetTable.Offsets[i] = minfReader.ReadUInt32();
+
+        minfReader.Position = BaseAddress + Marshal.OffsetOf<MINFInfo>("InstrumentInfoTableOffset") + Info.InstrumentInfoTableOffset;
+
+        InstrumentTable.InstrumentCount = minfReader.ReadUInt16();
+        InstrumentTable.Reserve0 = minfReader.ReadUInt16();
+
+        if (InstrumentTable.InstrumentCount > 0)
+        {
+            InstrumentTable.InstrumentInfo = new MINFInstrumentInfo[InstrumentTable.InstrumentCount];
+
+            for (int i = 0; i < InstrumentTable.InstrumentCount; i++)
+            {
+                long InstrumentAddress = minfReader.Position;
+
+                InstrumentTable.InstrumentInfo[i] = new MINFInstrumentInfo()
+                {
+                    Reserve0 = minfReader.ReadUInt32(),
+                    InstrumentOffset = minfReader.ReadUInt32()
+                };
+
+                minfReader.Position = InstrumentAddress + 4 + InstrumentTable.InstrumentInfo[i].InstrumentOffset;
+                
+                InstrumentTable.InstrumentInfo[i].Instrument = new MINFInstrument() 
+                {
+                    InstrumentNameOffset = minfReader.ReadUInt32(),
+                    Reserve0 = minfReader.ReadUInt16(),
+                    Reserve1 = minfReader.ReadUInt16(),
+                    Reserve2 = minfReader.ReadUInt32(),
+                };
+
+                
+            }
+
+            // for (int i = 0; i < InstrumentTable.InstrumentCount; i++)
+            // {
+            //     minfReader.Position = BaseAddress + InstrumentTable.Instruments[i].InstrumentNameOffset;
+            //     InstrumentTable.InstrumentInfo[i].InstrumentName = minfReader.ReadTerminatedString();
+            // }
+        }
+        else
+        {
+            Console.WriteLine("MINF contains no instruments!");
+        }
+
+        
+        
     }
+
+    
 }
