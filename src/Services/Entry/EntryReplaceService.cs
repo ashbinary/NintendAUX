19b2a8d9d8c4fa8a83890e792e56590c94eab3ff
ash <111416629+ashbinary@users.noreply.github.com>
2025-03-17 03:11:29 -0700

    Fix Prefetch lacking null check

--- src/Services/Entry/EntryReplaceService.cs
+++ src/Services/Entry/EntryReplaceService.cs
@@ -18,21 +18,23 @@ public class EntryReplaceService
     {
         var newBwav = await EntryCreateService.CreateBwav();
 
-        newBwav.Header.IsPrefetch = 1; // true
-        
-        // Slice it down to 0.3 seconds for accurate prefetching
-        for (int channel = 0; channel < newBwav.ChannelInfoArray.Length; channel++)
-        {
-            var sampleCount = (int)(Convert.ToInt32(newBwav.ChannelInfoArray[channel].SampleRate) * 0.3);
-
-            newBwav.ChannelInfoArray[channel].SampleCount = sampleCount;    
-            newBwav.ChannelInfoArray[channel].NonPrefetchSampleCount = sampleCount;
-            
-            newBwav.ChannelInfoArray[channel].OSamples = newBwav.ChannelInfoArray[channel].OSamples[..sampleCount];
-        }
-        
         if (MiscUtilities.CheckMagic(newBwav.Header.Magic, InputFileType.Bwav))
+        {
+            newBwav.Header.IsPrefetch = 1; // true
+        
+            // Slice it down to 0.3 seconds for accurate prefetching
+            for (int channel = 0; channel < newBwav.ChannelInfoArray.Length; channel++)
+            {
+                var sampleCount = (int)(Convert.ToInt32(newBwav.ChannelInfoArray[channel].SampleRate) * 0.3);
+
+                newBwav.ChannelInfoArray[channel].SampleCount = sampleCount;    
+                newBwav.ChannelInfoArray[channel].NonPrefetchSampleCount = sampleCount;
+            
+                newBwav.ChannelInfoArray[channel].OSamples = newBwav.ChannelInfoArray[channel].OSamples[..sampleCount];
+            }
+        
             ViewModelLocator.Model.InputFile.UpdateBwavAt(nodeIndex, newBwav);
+        }
     }
 
     public static async Task ReplaceBameta(int nodeIndex)
